9f1b5de11ddc7d31003aa9058ad3cd95fb863ed4
Jakub Meinlschmidt <jakub.meinlschmidt@icloud.com>
2023-07-02 22:18:10 +0200

    fix: Types in markInput

--- lib/rxjs-fsm/mark-input.ts
+++ lib/rxjs-fsm/mark-input.ts
@@ -1,6 +1,4 @@
 import { map, pipe } from 'rxjs';
 import { Input } from './models';
 
-const markInput = <T extends Input>(label: T) => pipe(map<void, T>(() => label));
-
-export default markInput;
+export const markInput = <T, R extends Input>(label: R) => pipe(map<T, R>(() => label));
